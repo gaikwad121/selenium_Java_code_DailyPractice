dd934985d60ded416a712ff28c366e3d64e7de2d
Jyoti Gaikwad <98073563+gaikwad121@users.noreply.github.com>
2025-08-17 00:19:16 +0530

    Add files via upload
    Added

---
+++ selenium/src/test/java/selenium/baseClass.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class baseClass {
+    static WebDriver driver;
+
+    @BeforeMethod
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(); 
+        driver.get("https://ui.vision/demo/webtest/frames/");
+        driver.manage().window().maximize();
+    }
+}
